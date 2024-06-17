6ae39a1c01ac4dfaf475fc02334c892fd73216cd
tuanvt14 <tuanvt14@fpt.com.vn>
2024-06-17 14:04:47 +0700

    quick update

--- pages/pac/config.js
+++ pages/pac/config.js
@@ -1,12 +1,13 @@
 function FindProxyForURL(url, host) {
   alert(`hello ${host} - ${url}`);
-  [
-    "*vlxx.mobi",
-    "gaito.shop",
-    "medium.com",
-    ".*medium.com",
-    ".*whatismyip.com"
-  ].some(p => shExpMatch(host, p)) {
+
+  if (
+    shExpMatch(host, "*vlxx.mobi" ) || 
+    shExpMatch(host, "gaito.shop" ) ||
+    shExpMatch(host, "medium.com" ) ||
+    shExpMatch(host, ".*medium.com" ) ||
+    shExpMatch(host, ".*whatismyip.com" )
+  ) {
     return "PROXY 127.0.0.1:65000";
   }
 
