c1c2bc65b16f31aa7820580af395d2694c6b4954
tuanvt14 <tuanvt14@fpt.com.vn>
2024-06-17 13:52:42 +0700

    quick update

--- pages/pac/config.js
+++ pages/pac/config.js
@@ -2,7 +2,8 @@ function FindProxyForURL(url, host) {
   alert(`hello ${host} - ${url}`);
   [
     "*vlxx.mobi",
-    "gaito.shop"
+    "gaito.shop",
+    "*medium.com"
   ].some(p => shExpMatch(host, p)) {
     return "PROXY 127.0.0.1:65000";
   }
